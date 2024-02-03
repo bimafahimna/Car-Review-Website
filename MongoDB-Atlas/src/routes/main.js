5d1233c2618a801b39ba0b1cc3551496905366f8
XFusion <bima.fahimna51@gmail.com>
2024-02-03 13:25:21 +0700

    new manufacturer routes added

--- MongoDB-Atlas/src/routes/main.js
+++ MongoDB-Atlas/src/routes/main.js
@@ -1,20 +1,28 @@
 const express = require('express')
-const { createMovie, getMovies, getMovieByid, updateMovie, deleteMovie } = require('../controllers/movieController')
-const { movieBodyMiddleware } = require('../middleware/movieMiddleware')
-const { register, login } = require('../controllers/authController')
+const { createCar, getCars, getCarById, updateCar, deleteMovie } = require('../controllers/carController')
+const { carBodyMiddleware,UpdateCarMiddleware } = require('../middleware/carMiddleware')
+const { register, login,deleteUser } = require('../controllers/userController')
+const { getManuf, createManuf } = require('../controllers/manufacturerController')
+
 const authenticateJWT = require('../middleware/jwtAuth')
 const router = express.Router()
-
-
-router.post('/api/movies',authenticateJWT,movieBodyMiddleware,createMovie)
-router.get('/api/movies', getMovies)
-router.get('/api/movies/:id', getMovieByid)
-router.patch('/api/movies/:id', updateMovie)
-router.delete('/api/movies/:id', deleteMovie)
 
 // auth
 router.post('/api/register', register)
 router.post('/api/login', login)
+router.delete("/api/delete",authenticateJWT,deleteUser)
+// router.get('/api/user',getUser)
+
+// Cars
+router.post('/api/cars',authenticateJWT,carBodyMiddleware,createCar)
+router.get('/api/cars', getCars)
+router.get('/api/car/:id', getCarById)
+router.patch('/api/car/:id',authenticateJWT,UpdateCarMiddleware, updateCar)
+router.delete('/api/car/:id',authenticateJWT, deleteMovie)
+
+// Manufacturer
+router.get('/api/manufacturers',getManuf)
+router.post('/api/manufacturers',createManuf)
 
 
 module.exports = router
